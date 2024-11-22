641844f3eba32e201df1b09fbf1bbc4438e4d2ce
myungkeun02 <audrms3568@gmail.com>
2024-11-23 08:32:49 +0900

    Expanded with daanggn layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,39 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            Flexible(child: Container(color: Colors.redAccent,), flex: 1,),
-            Flexible(child: Container(color: Colors.orangeAccent,), flex: 1,),
-            Flexible(child: Container(color: Colors.yellowAccent,), flex: 1,),
-            Flexible(child: Container(color: Colors.greenAccent,), flex: 1,),
-            Flexible(child: Container(color: Colors.blueAccent,), flex: 1,),
-            Flexible(child: Container(color: Colors.purpleAccent,), flex: 1,),
-          ],
+        appBar: AppBar(
+
         ),
-        bottomNavigationBar: BottomAppBar(),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          height: 150,
+          child: Row(
+            children: [
+              Image.asset("bej.png", width: 150,),
+              Expanded(
+                  child: Container(
+                    child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('카메라팝니다', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),),
+                        Text('금호동 3가', style: TextStyle(fontSize: 15, color: Colors.grey),),
+                        Text('7000원', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w900),),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.favorite_border),
+                            Text("4")
+                        ],
+                        )
+                      ],
+                    ),
+                  )
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+
+        ),
       )
     );
   }
