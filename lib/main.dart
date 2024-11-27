fb8ad63a29ef4afea9be407e93b0e9488a7c5f3f
myungkeun02 <audrms3568@gmail.com>
2024-11-27 22:58:15 +0900

    contact application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(
@@ -20,6 +21,7 @@ class _MyAppState extends State<MyApp> {
 
   var total = 3;
   var names = ["김영숙", "최명근", "피자집"];
+  var phoneNumbers = ["01012341234", '01050023568', '01033333333'];
   var likes = [0,0,0];
 
   addOne() {
@@ -28,9 +30,10 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  addName(name) {
+  addContact(name, phoneNumber) {
     setState(() {
       names.add(name);
+      phoneNumbers.add(phoneNumber);
     });
   }
 
@@ -46,13 +49,30 @@ class _MyAppState extends State<MyApp> {
           return ListTile(
             leading: Icon(Icons.account_circle),
             title: Text(names[i]),
+            subtitle: Text(phoneNumbers[i]),
+            trailing: ElevatedButton(
+              onPressed: (){
+                setState(() { // 상태 변경 알림
+                  names.removeAt(i);
+                  phoneNumbers.removeAt(i);// 인덱스를 기반으로 삭제
+                });
+              },
+              child: Text('삭제'),
+            ),
           );
           },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: ElevatedButton(onPressed: (){
+          setState(() {
+            names.sort();
+          });
+        }, child: Text("정렬")),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(context: context, builder: (context){
-            return DialogUI(addOne : addOne, addName: addName,);
+            return DialogUI(addOne : addOne, addContact: addContact,);
           });
         },
       ),
@@ -61,12 +81,12 @@ class _MyAppState extends State<MyApp> {
 }
 
 class DialogUI extends StatelessWidget {
-  DialogUI({super.key, this.addOne, this.addName});
+  DialogUI({super.key, this.addOne, this.addContact});
 
   final addOne;
-  final addName;
-  var inputData = '';
-
+  final addContact;
+  var name = TextEditingController();
+  var phoneNumber = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -78,15 +98,18 @@ class DialogUI extends StatelessWidget {
         child: Column(
           children: [
             TextField(
-              onChanged: (text) {
-                  inputData = text;
-              },
+              controller: name,
+            ),
+            TextField(
+              controller: phoneNumber,
             ),
             TextButton(
                 onPressed: (){
                   addOne();
-                  addName(inputData);
-                  Navigator.pop(context);
+                  if (name.text != '' || phoneNumber.text != '') {
+                    addContact(name.text, phoneNumber.text);
+                    Navigator.pop(context);
+                  }
                 },
                 child: Text("완료")
             ),
